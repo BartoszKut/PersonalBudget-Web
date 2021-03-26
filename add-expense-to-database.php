71903ddc95d0572a474e93483cb1b92549c312cc
Bartosz Kut <bartosz.kut93@gmail.com>
2021-03-26 11:47:47 +0100

    Finished added incomes and expenses to database

--- add-expense-to-database.php
+++ add-expense-to-database.php
@@ -3,36 +3,76 @@
     session_start();
 
     require_once "db_php_connection.php"; //this file opens connection with MySQL server
-
     if(!($_POST['amount']=="")){
         $all_ok = true; //correct validation
 
-        $logged_user_id = $_SESSION['logged_user_id'];
-        $amount = $_POST['amount']; 
+        $logged_user_id = $_SESSION['logged_user_id'];        
+        $comment = $_POST['comment'];//no validation, in comment user can writes everything
+
+        //amount correctness check
+        $amount = $_POST['amount'];
+        if((is_numeric($amount) == false) || ($amount < 0.01)){
+            $all_ok = false;
+            $_SESSION['e_amount'] = "Podaj prawidłową wartość przychodu.";
+        }
+
+        //date correctness check
         $date = $_POST['date'];
-        $paymentmethod = $_POST['paymentmethod'];
+        $Date = strtotime($_POST['date']);    
+        $timestamp = $Date; 
+        $day=date('d',$timestamp);
+        $month=date('m',$timestamp);
+        $year=date('Y',$timestamp);
+        if(!checkdate($month, $day, $year)){
+            $all_ok = false;
+            $_SESSION['e_date'] = "Wprowadź poprawną datę.";
+        }
+
+        //category correctness check
         $expensecat = $_POST['expensecat'];
-        $comment = $_POST['comment'];
-       
+        if($expensecat==""){
+            $all_ok = false;
+            $_SESSION['e_expensecat'] = "Wybierz kategorię wydatku.";
+        }   
 
-        $add_expense_to_database = $database->prepare("INSERT INTO expenses (user_id, expense_category_assigned_to_user_id, payment_method_assigned_to_user_id, amount, date_of_expense, expense_comment) VALUES (:user_id, (SELECT id FROM expenses_category_assigned_to_users WHERE user_id = :user_id AND name = :expensecat), (SELECT id FROM payment_methods_assigned_to_users WHERE user_id = :user_id AND name = :paymentmethod), :amount, :date, :comment)");
+        //payment method correctness check
+        $paymentmethod = $_POST['paymentmethod'];
+        if($paymentmethod==""){
+            $all_ok = false;
+            $_SESSION['e_paymentmethod'] = "Wybierz sposób płatności.";
+        }   
+
+        //tests pass, income added
+        if($all_ok == true){
+            //query -> get expense category id
+            $select_expense_category_id = $database->prepare("SELECT id FROM expenses_category_assigned_to_users WHERE user_id = :user_id AND name = :expensecat");
+            $select_expense_category_id->bindValue(':user_id', $logged_user_id, PDO::PARAM_INT);
+            $select_expense_category_id->bindValue(':expensecat', $expensecat, PDO::PARAM_INT);
+            $select_expense_category_id->execute();
+            $category_id = $select_expense_category_id->fetchColumn();
 
 
-        $add_expense_to_database->bindParam(':user_id', $logged_user_id, PDO::PARAM_INT);
-        echo $logged_user_id."<br/><br/>"; 
-        $add_expense_to_database->bindParam(':expensecat', $expensecat, PDO::PARAM_STR);
-        echo $expensecat."<br/><br/>";
-        $add_expense_to_database->bindParam(':paymentmethod', $paymentmethod, PDO::PARAM_STR);
-        echo "$paymentmethod<br/><br/>";
-        $add_expense_to_database->bindValue(':amount', $amount);
-        echo "$amount<br/><br/>";
-        $add_expense_to_database->bindValue(':date', $date);
-        echo $date."<br/><br/>";
-        $add_expense_to_database->bindParam(':comment', $comment, PDO::PARAM_STR);
-        echo $comment."<br/><br/>";
+            //query -> get payment method id
+            $select_payment_method_id = $database->prepare("SELECT id FROM payment_methods_assigned_to_users WHERE user_id = :user_id AND name = :paymentmethod");
+            $select_payment_method_id->bindValue(':user_id', $logged_user_id, PDO::PARAM_INT);
+            $select_payment_method_id->bindValue(':paymentmethod', $paymentmethod, PDO::PARAM_STR);
+            $payment_id = $select_payment_method_id->fetchColumn();
+            echo "Platnosc id dziala <br>";
 
-        $add_expense_to_database->execute();
-        echo "<br/>powinien wykonac"; 
 
-        header('Location: main-menu.php');
+            //query -> add expense to database
+            $add_expense_to_database = $database->prepare("INSERT INTO expenses (id, user_id, expense_category_assigned_to_user_id, payment_method_assigned_to_user_id, amount, date_of_expense, expense_comment) VALUES (NULL, :user_id, :category_id, :payment_id, :amount, :date, :comment)");
+            echo "query dziala<br>";
+            $add_expense_to_database->bindParam(':user_id', $logged_user_id, PDO::PARAM_INT);
+            $add_expense_to_database->bindParam(':category_id', $category_id, PDO::PARAM_INT);
+            $add_expense_to_database->bindParam(':payment_id', $payment_id, PDO::PARAM_INT);
+            $add_expense_to_database->bindValue(':amount', $amount, PDO::PARAM_STR);
+            $add_expense_to_database->bindValue(':date', $date, PDO::PARAM_STR);
+            $add_expense_to_database->bindParam(':comment', $comment, PDO::PARAM_STR);
+            $add_expense_to_database->execute();
+
+            
+            $_SESSION['expense_success'] = "Dodano wydatek.";
+            header('Location: main-menu.php');
+        }
     }
--- add-income-to-database.php
+++ add-income-to-database.php
@@ -7,27 +7,57 @@
     if(!($_POST['amount']=="")){
         $all_ok = true; //correct validation
 
-        $logged_user_id = $_SESSION['logged_user_id'];
-        $amount = $_POST['amount']; 
+        $logged_user_id = $_SESSION['logged_user_id'];           
+        $comment = $_POST['comment'];//no validation, in comment user can writes everything
+
+        //amount correctness check
+        $amount = $_POST['amount'];
+        if((is_numeric($amount) == false) || ($amount < 0.01)){
+            $all_ok = false;
+            $_SESSION['e_amount'] = "Podaj prawidłową wartość przychodu.";
+        }
+
+        //date correctness check
         $date = $_POST['date'];
+        $Date = strtotime($_POST['date']);    
+        $timestamp = $Date; 
+        $day=date('d',$timestamp);
+        $month=date('m',$timestamp);
+        $year=date('Y',$timestamp);
+        if(!checkdate($month, $day, $year)){
+            $all_ok = false;
+            $_SESSION['e_date'] = "Wprowadź poprawną datę.";
+        }
+
+        //category correctness check
         $incomecat = $_POST['category'];
-        $comment = $_POST['comment'];
+        if($incomecat==""){
+            $all_ok = false;
+            $_SESSION['e_incomecat'] = "Wybierz kategorię przychodu.";
+        }   
 
-        $add_income_to_database = $database->prepare("INSERT INTO incomes (id, user_id, income_category_assigned_to_user_id, amount, date_of_income, income_comment) VALUES (NULL, :user_id, (SELECT id FROM incomes_category_assigned_to_users WHERE user_id = :user_id AND name = :incomecat), :amount, :date, :comment)");
+        //tests pass, income added
+        if($all_ok == true){
 
-        $add_income_to_database->bindParam(':user_id', $logged_user_id, PDO::PARAM_INT);
-        echo $logged_user_id."<br/><br/>"; 
-        $add_income_to_database->bindParam(':incomecat', $incomecat, PDO::PARAM_STR);
-        echo $incomecat."<br/><br/>";
-        $add_income_to_database->bindValue(':amount', $amount);
-        echo "$amount<br/><br/>";
-        $add_income_to_database->bindValue(':date', $date);
-        echo $date."<br/><br/>";
-        $add_income_to_database->bindParam(':comment', $comment, PDO::PARAM_STR);
-        echo $comment."<br/><br/>";
+            //query -> get income category id 
+            $select_income_category_id = $database->prepare("SELECT id FROM incomes_category_assigned_to_users WHERE name = :incomecat AND user_id = :user_id");
+            $select_income_category_id->bindValue(':incomecat', $incomecat, PDO::PARAM_STR);
+            $select_income_category_id->bindValue(':user_id', $logged_user_id, PDO::PARAM_INT);
+            $select_income_category_id->execute();
+            $category_id = $select_income_category_id->fetchColumn();
 
-        $add_income_to_database->execute();
-        echo "<br/>powinien wykonac"; 
+            
+            //query -> add income to database
+            $add_income_to_database = $database->prepare("INSERT INTO incomes (id, user_id, income_category_assigned_to_user_id, amount, date_of_income, income_comment) VALUES (NULL, :user_id, :category_id, :amount, :date, :comment)");
+            $add_income_to_database->bindValue(':user_id', $logged_user_id, PDO::PARAM_INT);
+            $add_income_to_database->bindValue(':category_id', $category_id, PDO::PARAM_INT);
+            $add_income_to_database->bindValue(':amount', $amount, PDO::PARAM_STR);
+            $add_income_to_database->bindValue(':date', $date, PDO::PARAM_STR);
+            $add_income_to_database->bindValue(':comment', $comment, PDO::PARAM_STR);
+            $add_income_to_database->execute();  
 
-        header('Location: main-menu.php');
+
+            $_SESSION['income_success'] = "Dodano przychód.";
+            header('Location: main-menu.php');
+        }       
     }
